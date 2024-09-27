6ec2d70806bd3e86e4eedf87cef887312e0a053d
Rohanbasnet12 <rohanbmyname@gmail.com>
2024-09-26 23:30:40 -0700

    First commit

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div id="app">
+      <h1 className="text-2xl font-bolder w-full text-center">
+        This is an todo application
+      </h1>
+    </div>
+  );
+}
+
+export default App;
